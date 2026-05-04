5081d66f88530b4b9f3c59c05f957b3812648d36
Rouf <rouf@example.com>
2026-05-04 09:45:02 -0400

    Initial implementation of RP2040 Web Serial Bridge

--- build/CMakeFiles/web_serial_bridge.dir/home/rouf/hardware-project/rp2040-usb-to-ttl/sdk/pico-sdk/lib/tinyusb/src/class/midi/midi_device.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/web_serial_bridge.dir/home/rouf/hardware-project/rp2040-usb-to-ttl/sdk/pico-sdk/lib/tinyusb/src/class/midi/midi_device.c.o: \
- /home/rouf/hardware-project/rp2040-usb-to-ttl/sdk/pico-sdk/lib/tinyusb/src/class/midi/midi_device.c \
- /home/rouf/hardware-project/rp2040-usb-to-ttl/sdk/pico-sdk/lib/tinyusb/src/tusb_option.h \
- /home/rouf/hardware-project/rp2040-usb-to-ttl/sdk/pico-sdk/lib/tinyusb/src/common/tusb_compiler.h \
- /home/rouf/hardware-project/rp2040-web-serial-bridge/include/tusb_config.h \
- /home/rouf/hardware-project/rp2040-usb-to-ttl/sdk/pico-sdk/lib/tinyusb/src/common/tusb_mcu.h
